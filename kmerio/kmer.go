a35134a69cc732f33b204f8f905b5edd461a4d68
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2018-07-07 01:52:13 -0700

    move kmer operations to one file, transform kmers from string to int slice

--- kmerio/kmer.go
+++ kmerio/kmer.go
@@ -1,7 +1,12 @@
 package kmerio
 
 import (
-	// "github.com/emirpasic/gods/sets/hashset"
+	"os"
+	"bufio"
+	"strings"
+	"fmt"
+
+	"github.com/emirpasic/gods/sets/hashset"
 )
 
 func checkError(e error) {
@@ -10,14 +15,56 @@ func checkError(e error) {
 	}
 }
 
-func getKmersFromString(k int, seq []byte) [][]byte {
+func ParseFastQ(filepath string, k int) {
+	f, err := os.Open(filepath)
+	checkError(err)
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	var kmers [][]int
+	i := 0
+
+	for s.Scan() {
+		if (i - 1) % 4 == 0 {
+			sr := strings.ToUpper(s.Text())
+
+			for _, kmer := range GetKmersFromShortRead(k, []byte(sr)) {
+				kmers = append(kmers, kmer)
+			}
+		}
+		i++
+	}
+
+	fmt.Println(len(kmers))
+}
+
+func GetKmersFromShortRead(k int, seq []byte) [][]int {
 	num_kmers := len(seq) - k + 1
-	kmers := make([][]byte, num_kmers)
+	kmers := make([][]int, num_kmers)
 
 	for i := range kmers {
-		kmers[i] = make([]byte, k)
-		copy(kmers[i], seq[i : i + k])
+		kmers[i] = NucleotideToInt(seq[i : i + k])
 	}
 
 	return kmers
+}
+
+func NucleotideToInt(kmer []byte) []int {
+	new_kmer := make([]int, len(kmer))
+
+	for i, nt := range kmer {
+		if nt == 'A' {
+			new_kmer[i] = 0
+		} else if nt == 'C' {
+			new_kmer[i] = 1
+		} else if nt == 'G' {
+			new_kmer[i] = 2
+		} else if nt == 'T' {
+			new_kmer[i] = 3
+		} else {
+			new_kmer[i] = 0
+		}
+	}
+
+	return new_kmer
 }
