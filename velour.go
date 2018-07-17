e286f0f301586e0ef864e945cc1a88eea7179429
Sachandhan Ganesh <sachan.ganesh@gmail.com>
2018-07-17 01:37:54 -0700

    comment out profiling tools

--- velour.go
+++ velour.go
@@ -5,13 +5,17 @@ import (
 
 	"velour/seqio"
 	"velour/test/time"
+
+	// "github.com/pkg/profile"
 )
 
 func main() {
+	// defer profile.Start().Stop()
+
 	fragments := []string{"data/staphylococcus_aureus/frag1.fastq", "data/staphylococcus_aureus/frag2.fastq"}
 
 	k := seqio.EstimateK(2900000)
 	fmt.Println("Estimated K:", k)
 
-	test_time.TimePASHAGraph(fragments[:1], k)
+	test_time.TimeAll(fragments[:1], k)
 }
